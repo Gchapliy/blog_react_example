bb23dd7a2f72e69769b90a57ab29718d7414f748
Gchapliy <grishachapliy1@gmail.com>
2020-06-10 15:41:07 +0300

    posts links

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,9 +8,9 @@ class FullPost extends Component {
     };
 
     componentDidUpdate() {
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         this.setState({loadedPost: response.data});
                     });
